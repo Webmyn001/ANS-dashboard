f34b3c7057ffb841f78ac2280fc8e5d74d4962af
Webmyn001 <bellomuhyideen0001@gmail.com>
2023-03-24 00:50:17 -0700

    first

--- src/ANSdashboard/Post.js
+++ src/ANSdashboard/Post.js
@@ -24,7 +24,21 @@ function Formdetails() {
   const [Body15, setBody15] = useState("")
   const [Body16, setBody16] = useState("")
 
-
+  const [Subtitle1, setSubtitle1] = useState("")
+  const [Subtitle2, setSubtitle2] = useState("")
+  const [Subtitle3, setSubtitle3] = useState("")
+  const [Subtitle4, setSubtitle4] = useState("")
+  const [Subtitle5, setSubtitle5] = useState("")
+  const [Subtitle6, setSubtitle6] = useState("")
+  const [Subtitle7, setSubtitle7] = useState("")
+  const [Subtitle8, setSubtitle8] = useState("")
+  const [Subtitle9, setSubtitle9] = useState("")
+  const [Subtitle10, setSubtitle10] = useState("")
+  const [Subtitle11, setSubtitle11] = useState("")
+  const [Subtitle12, setSubtitle12] = useState("")
+  const [Subtitle13, setSubtitle13] = useState("")
+  const [Subtitle14, setSubtitle14] = useState("")
+  const [Subtitle15, setSubtitle15] = useState("")
 
 
   const [images, setImages] = useState("")
@@ -40,7 +54,10 @@ function Formdetails() {
 
 
     axios.post("https://easy-crown-bull.cyclic.app/api/lessons/add", {Title,Subtitle,Body1,Body2,Body3,images,
-    Body4,Body5,Body6,Body7,Body8,Body9,Body10,Body11,Body12,Body13,Body14,Body15,Body16})
+    Body4,Body5,Body6,Body7,Body8,Body9,Body10,Body11,Body12,Body13,Body14,Body15,Body16,
+    Subtitle1,Subtitle2,Subtitle3,Subtitle4,Subtitle5,Subtitle6,Subtitle7,Subtitle8,Subtitle9,Subtitle10,Subtitle11,
+    Subtitle12,Subtitle13,Subtitle14,Subtitle15
+  })
     .then((res)=>
     { 
     console.log("saved succesfully")
@@ -134,6 +151,56 @@ const OnchangeBody15 =(e)=> {
 const OnchangeBody16 =(e)=> {
   setBody16(e.target.value)
 }
+
+
+const Onchangesubtitle1 =(e)=> {
+  setSubtitle1(e.target.value)
+}
+ 
+const Onchangesubtitle2 =(e)=> {
+  setSubtitle2(e.target.value)
+}
+const Onchangesubtitle3 =(e)=> {
+  setSubtitle3(e.target.value)
+}
+const Onchangesubtitle4 =(e)=> {
+  setSubtitle4(e.target.value)
+}
+const Onchangesubtitle5 =(e)=> {
+  setSubtitle5(e.target.value)
+}
+const Onchangesubtitle6 =(e)=> {
+  setSubtitle6(e.target.value)
+}
+const Onchangesubtitle7 =(e)=> {
+  setSubtitle7(e.target.value)
+}
+ const Onchangesubtitle8 =(e)=> {
+  setSubtitle8(e.target.value)
+}
+const Onchangesubtitle9 =(e)=> {
+  setSubtitle9(e.target.value)
+}
+const Onchangesubtitle10 =(e)=> {
+  setSubtitle10(e.target.value)
+}
+const Onchangesubtitle11 =(e)=> {
+  setSubtitle11(e.target.value)
+}
+const Onchangesubtitle12 =(e)=> {
+  setSubtitle12(e.target.value)
+}
+const Onchangesubtitle13 =(e)=> {
+  setSubtitle13(e.target.value)
+}
+const Onchangesubtitle14 =(e)=> {
+  setSubtitle14(e.target.value)
+}
+const Onchangesubtitle15 =(e)=> {
+  setSubtitle15(e.target.value)
+}
+
+
 const Onchangeimages = async (e) => { 
   const files = Array.from(e.target.files)
       
@@ -180,79 +247,137 @@ console.log(Title, Subtitle, Body1, Body2, Body3,Body4)
                 <input placeholder="Sub-title" value={Subtitle} onChange={Onchangesubtitle}
                 className=" border-[#0b0f11] w-[250px] focus:outline-0 border-2 mb-3 sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
 
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle1</label>
+                <input placeholder="Sub topic1" value={Subtitle1} onChange={Onchangesubtitle1}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
                 <label className="  text-[#7f030d] font-semibold pt-3 py-1 ">first body</label>
                 <textarea placeholder="First paragraph" value={Body1} onChange={OnchangeBody1}
                 className=" border-[#0b0f11] h-[100px] w-[250px] text-[#20201f] focus:outline-0 border-2 sm:w-[400px] rounded-lg px-3 "/>
 
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle2</label>
+                <input placeholder="Sub topic2" value={Subtitle2} onChange={Onchangesubtitle2}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
                 <label className=" text-[#7f030d]  font-semibold py-1 ">Second body</label>
                 <textarea  value={Body2} onChange={OnchangeBody2}  placeholder="Second paragraph"
                 className=" border-[#0b0f11] focus:outline-0 border-2
                  sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
-
+ 
+ <label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle3</label>
+                <input placeholder="Sub topic3" value={Subtitle3} onChange={Onchangesubtitle3}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
                 <label className=" text-[#7f030d] py-1 font-semibold">Third body</label>
                 <textarea  value={Body3} onChange={OnchangeBody3}  placeholder="Third paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
 
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle4</label>
+                <input placeholder="Sub topic4" value={Subtitle4} onChange={Onchangesubtitle4}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
                <label className=" text-[#7f030d] py-1 font-semibold">Fourth body</label>
                 <textarea  value={Body4} onChange={OnchangeBody4}  placeholder="Forth paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
 
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle5</label>
+                <input placeholder="Sub topic5" value={Subtitle5} onChange={Onchangesubtitle5}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
                <label className=" text-[#7f030d] py-1 font-semibold">Fifth body</label>
                 <textarea  value={Body5} onChange={OnchangeBody5}  placeholder="Fifth paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
 
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle6</label>
+                <input placeholder="Sub topic6" value={Subtitle6} onChange={Onchangesubtitle6}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
               <label className=" text-[#7f030d] py-1 font-semibold">Sixth body</label>
                 <textarea  value={Body6} onChange={OnchangeBody6}  placeholder="Sixth paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
 
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle7</label>
+                <input placeholder="Sub topic7" value={Subtitle7} onChange={Onchangesubtitle7}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
               <label className=" text-[#7f030d] py-1 font-semibold">Seventh body</label>
                 <textarea  value={Body7} onChange={OnchangeBody7}  placeholder="Seventh paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
 
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle8</label>
+                <input placeholder="Sub topic8" value={Subtitle8} onChange={Onchangesubtitle8}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
 <label className=" text-[#7f030d] py-1 font-semibold">8th body</label>
                 <textarea  value={Body8} onChange={OnchangeBody8}  placeholder="8th paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
 
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle9</label>
+                <input placeholder="Sub topic9" value={Subtitle9} onChange={Onchangesubtitle9}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
 <label className=" text-[#7f030d] py-1 font-semibold">9th body</label>
                 <textarea  value={Body9} onChange={OnchangeBody9}  placeholder=" 9th paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
 
-
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle10</label>
+                <input placeholder="Sub topic10" value={Subtitle10} onChange={Onchangesubtitle10}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
 <label className=" text-[#7f030d] py-1 font-semibold">10th body</label>
                 <textarea  value={Body10} onChange={OnchangeBody10}  placeholder="10th paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
-
+ 
+ <label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle11</label>
+                <input placeholder="Sub topic11" value={Subtitle11} onChange={Onchangesubtitle11}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
 
 <label className=" text-[#7f030d] py-1 font-semibold">11th body</label>
                 <textarea  value={Body11} onChange={OnchangeBody11}  placeholder=" 11th paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
 
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle12</label>
+                <input placeholder="Sub topic12" value={Subtitle12} onChange={Onchangesubtitle12}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
 
 <label className=" text-[#7f030d] py-1 font-semibold">12th body</label>
                 <textarea  value={Body12} onChange={OnchangeBody12}  placeholder="12th paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
 
-
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle13</label>
+                <input placeholder="Sub topic13" value={Subtitle13} onChange={Onchangesubtitle13}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
 <label className=" text-[#7f030d] py-1 font-semibold">13th body</label>
                 <textarea  value={Body13} onChange={OnchangeBody13}  placeholder=" 13th paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
 
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle14</label>
+                <input placeholder="Sub topic14" value={Subtitle14} onChange={Onchangesubtitle14}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
 <label className=" text-[#7f030d] py-1 font-semibold">14th body</label>
                 <textarea  value={Body14} onChange={OnchangeBody14}  placeholder="14th paragraph"
                 className=" border-[#0b0f11] focus:outline-0
                  border-2 sm:w-[400px] w-[250px] h-[100px] rounded-lg px-3 text-[#20201f]"/>
 
+<label className="font-semibold text-[#7f030d] pt-3  text-xl py-1 ">Subtitle15</label>
+                <input placeholder="Sub topic15" value={Subtitle15} onChange={Onchangesubtitle15}
+                className=" border-[#5a5542] focus:outline-0 border-2 mb-3 w-[250px] sm:w-[400px] rounded-lg px-3 text-[#594545]"/>
+                
 <label className=" text-[#7f030d] py-1 font-semibold">15th body</label>
                 <textarea  value={Body15} onChange={OnchangeBody15}  placeholder="15th paragraph"
                 className=" border-[#0b0f11] focus:outline-0
